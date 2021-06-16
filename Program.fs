c0bf5aae75db8e8d2b904ab18f3e3f2d49f386f9
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-06-16 18:51:28 +0200

    wip

---
+++ Program.fs
@@ -0,0 +1,37 @@
+module Server
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+open HttpHandlers
+
+let routes =
+    choose [ GET    >=> route  "/recipes"    >=> getRecipes ()
+             POST   >=> route  "/recipe"     >=> postRecipe
+             PUT    >=> route  "/recipe"     >=> putRecipe
+             DELETE >=> routef "/recipe/%O" deleteRecipe
+             RequestErrors.NOT_FOUND "Not found"
+           ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe routes
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main args =
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(Action<IApplicationBuilder> configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
