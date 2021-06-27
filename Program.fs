f92cde9b704d15e9c9258bb477568a3e09dcec9f
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-06-27 12:26:37 +0200

    Update recipes

--- Program.fs
+++ Program.fs
@@ -1,21 +1,23 @@
 type Measurement = 
-    | Kg of float
-    | G of float
-    | Mg of float
-    | L of float
-    | Dl of float
-    | Ml of float
-    | Ms of float
-    | Ss of float
-    | Ts of float
-    | Stk of float
+    | Kg
+    | G 
+    | Mg 
+    | L 
+    | Dl 
+    | Ml
+    | Ms 
+    | Ss 
+    | Ts 
+    | Stk 
 
 type Ingredient =
-    { Measurement: Measurement
+    { Volume: float
+      Measurement: Measurement
       Name: string }
-      
+
 let ingredient volume measurement name = 
-    { Measurement = measurement volume
+    { Volume = volume
+      Measurement = measurement 
       Name = name }
 
 type Meal =
@@ -27,19 +29,50 @@ type Meal =
 type Recipe =
     { Id: System.Guid
       Title: string
+      Description: string
       Meal: Meal
       Time: float
       Steps: string list
       Ingredients: Ingredient list
-      Portions: int
-      SubRecipes: System.Guid list }
+      Portions: int }
 
-let createRecipe title meal time steps ingredients portions subRecipes =
+let createRecipe title description meal time steps ingredients portions =
     { Id = System.Guid.NewGuid()
       Title = title
+      Description = description
       Meal = meal
       Time = time
       Steps = steps
       Ingredients = ingredients
-      Portions = portions
-      SubRecipes = List.map (fun r -> r.Id) subRecipes }
+      Portions = portions }
+
+let koktPotet = 
+  createRecipe 
+     "Kokt potet"
+     "En skikkelig, potensielt smakløs, klassiker som du som inngår i ganske mange andre retter."
+     Dinner
+     20.
+     [ "Skrubb og skyll potetene"
+       "Del potetene i 2"
+       "Kok dem i 10-15 minutter til de er gjennomkokte" ]
+     [ ingredient 800. G "Potet"
+       ingredient 1. L "Vann"
+       ingredient 1. Ts "Salt" ]
+     4
+
+let pyttIPanne =
+  createRecipe
+    "Pytt i panne"
+    "Det evige hvilestedet til gamle middager."
+    Dinner
+    20.
+    [ "Stek baconet og del det inn i biter"
+      "Del potetene og inn i terninger og hakk løk."
+      "Stek poteten og løken sammen i bacon-fettet"
+      "Bland inn baconet"
+      "Del paprikaen i biter og dryss over"]
+    [ ingredient 2. Stk "Bacon"
+      ingredient 4. Stk "Kokte poteter"
+      ingredient 1. Stk "Løk"
+      ingredient 0.25 Stk "Paprika"]
+      2
