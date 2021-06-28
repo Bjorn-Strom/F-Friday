3589cec131669b5d172d777969955698ec6667bf
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-06-28 18:19:37 +0200

    2-recipes

--- Program.fs
+++ Program.fs
@@ -1,37 +1,83 @@
-module Server
+type Measurement = 
+    | Kg
+    | G 
+    | Mg 
+    | L 
+    | Dl 
+    | Ml
+    | Ms 
+    | Ss 
+    | Ts 
+    | Stk 
 
-open System
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.DependencyInjection
-open Giraffe
+type Ingredient =
+    { Amount: float
+      Measurement: Measurement
+      Name: string }
 
-open HttpHandlers
+let ingredient amount measurement name = 
+    { Amount = amount
+      Measurement = measurement 
+      Name = name }
 
-let routes =
-    choose [ GET    >=> route  "/recipes"    >=> getRecipes ()
-             POST   >=> route  "/recipe"     >=> postRecipe
-             PUT    >=> route  "/recipe"     >=> putRecipe
-             DELETE >=> routef "/recipe/%O" deleteRecipe
-             RequestErrors.NOT_FOUND "Not found"
-           ]
+type Meal =
+    | Breakfast
+    | Lunch
+    | Dinner
+    | Desert
 
-let configureApp (app : IApplicationBuilder) =
-    app.UseGiraffe routes
+type Time =
+    | Minutes of int
+    | Hours of float
 
-let configureServices (services : IServiceCollection) =
-    services.AddGiraffe() |> ignore
+type Portions = Portions of int
+type Recipe =
+    { Id: System.Guid
+      Title: string
+      Description: string
+      Meal: Meal
+      Time: Time
+      Steps: string list
+      Ingredients: Ingredient list
+      Portions: Portions }
 
-[<EntryPoint>]
-let main args =
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .Configure(Action<IApplicationBuilder> configureApp)
-                    .ConfigureServices(configureServices)
-                    |> ignore)
-        .Build()
-        .Run()
-    0
+let createRecipe meal title description time steps ingredients portions =
+    { Id = System.Guid.NewGuid()
+      Title = title
+      Description = description
+      Meal = meal
+      Time = time
+      Steps = steps
+      Ingredients = ingredients
+      Portions = portions }
+
+let koktPotet = 
+  createRecipe 
+     Dinner
+     "Kokt potet"
+     "En skikkelig, potensielt smakløs, klassiker som du som inngår i ganske mange andre retter."
+     (Minutes 20)
+     [ "Skrubb og skyll potetene"
+       "Del potetene i 2"
+       "Kok dem i 10-15 minutter til de er gjennomkokte" ]
+     [ ingredient 800. G "Potet"
+       ingredient 1. L "Vann"
+       ingredient 1. Ts "Salt" ]
+     (Portions 4)
+
+let pyttIPanne =
+  createRecipe
+    Dinner
+    "Pytt i panne"
+    "Det evige hvilestedet til gamle middager."
+    (Minutes 20)
+    [ "Stek baconet og del det inn i biter"
+      "Del potetene og inn i terninger og hakk løk."
+      "Stek poteten og løken sammen i bacon-fettet"
+      "Bland inn baconet"
+      "Del paprikaen i biter og dryss over"]
+    [ ingredient 2. Stk "Bacon"
+      ingredient 4. Stk "Kokte poteter"
+      ingredient 1. Stk "Løk"
+      ingredient 0.25 Stk "Paprika"]
+    (Portions 2)
