3da7c5ccadbde35eaffd06b8a34231e92eb1cf42
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-07-01 20:49:20 +0200

    Fine tune

--- Program.fs
+++ Program.fs
@@ -5,23 +5,32 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Cors.Infrastructure
 open Giraffe
+open Thoth.Json.Net
 
 open HttpHandlers
 
 let routes =
-    choose [ GET    >=> route  "/recipes"    >=> getRecipes 
-             POST   >=> route  "/recipe"     >=> postRecipe
-             PUT    >=> route  "/recipe"     >=> putRecipe
-             DELETE >=> routef "/recipe/%O" deleteRecipe
+    choose [ GET    >=> route  "/api/recipes"    >=> getRecipes
+             POST   >=> route  "/api/recipe"     >=> postRecipe
+             PUT    >=> route  "/api/recipe"     >=> putRecipe
+             DELETE >=> routef "/api/recipe/%O" deleteRecipe
              RequestErrors.NOT_FOUND "Not found"
            ]
+
+let configureCors (builder: CorsPolicyBuilder) =
+    builder.AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowAnyOrigin() |> ignore
 
 let configureApp (app : IApplicationBuilder) =
     app.UseGiraffe routes
 
 let configureServices (services : IServiceCollection) =
+    services.AddCors() |> ignore
     services.AddGiraffe() |> ignore
+    services.AddSingleton<Json.ISerializer> (Thoth.Json.Giraffe.ThothSerializer (caseStrategy = CamelCase)) |> ignore
 
 [<EntryPoint>]
 let main args =
